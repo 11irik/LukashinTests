903fc42e0373368ade09136cec1791ef8d311427
11irik <lykashin.kirill@gmail.com>
2019-11-27 02:23:20 +0400

    testtest

--- src/test/java/ru/beru/tests/BuyToothbrushes.java
+++ src/test/java/ru/beru/tests/BuyToothbrushes.java
@@ -20,7 +20,7 @@ public class BuyToothbrushes extends WebDriverSettings {
         electricalToothbrushesPage.open();
         electricalToothbrushesPage.setStartPrice(1000);
 
-
+        int a = 2;
 //        for(int i = 0; i < 100000; ++i) {
 //            System.out.print(" ");
 //        }
