06dd75e9cfac5ce7dd1fcfbe80afbafc08fc8718
Kirill <lukashinkv@gmail.com>
2019-12-04 04:34:44 +0400

    added testng.xml

---
+++ src/test/java/ru/beru/dataProviders/ChangeCityTestDataProvider.java
@@ -0,0 +1,13 @@
+package ru.beru.dataProviders;
+
+import org.testng.annotations.DataProvider;
+
+public class ChangeCityTestDataProvider {
+    @DataProvider(name = "data-provider")
+    public static Object[][] dataProviderMethod() {
+        return new Object[][]{ {"Саратов", "Хвалынск"}, { "Саратов", "Самара"} , {"Саратов", "Москва"}};
+    }
+}
+
+
+
